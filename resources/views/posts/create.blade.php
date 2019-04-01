2cdeceff1a6e4ed50a0f1724ee21ffde2c65c586
EC2 Default User <ec2-user@ip-172-31-43-230.us-east-2.compute.internal>
2019-04-01 23:09:56 +0000

    clean the look

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,17 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>投稿ページ</h2>
-    <form action="{{ action('PostContller@store') }}" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <div>タイトル</div>
-        <div><input type="text" name="title" value="{{ old('title') }}" /></div>
-        <div>投稿</div>
-        <div><textarea name="body" value="{{ old('body') }}" rows="20"/></textarea></div>
-        <div><input type="file" name="image"/></div>
-        
-        <input type="hidden" name="user_id" value="{{ $user->id }}"/>
-        
-        <input type="submit" value="投稿"/>
-    </form>
+<div class="container">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">投稿ページ</div>
+            <div class="card-body">
+                <form action="{{ action('PostContller@store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div>
+                        タイトル
+                    </div>
+                    <div class='col-md-8 form-group'>
+                        <input type="text" class="form-control" name="title" value="{{ old('title') }}" />
+                    </div>
+                    <div>
+                        本文
+                    </div>
+                    <div class='col-md-8 form-group'>
+                        <textarea name="body" class="form-control" value="{{ old('body') }}" rows="20"></textarea>
+                    </div>
+                    
+                    <label>
+                        <span class="btn btn-dark">
+                            画像を添付
+                            <input type="file" name="image" style="display:none">
+                        </span>
+                    </label>
+                    
+                    <button type="submit" class="btn btn-danger"/>投稿</button>
+    
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <table>
-        @foreach($posts as $post)
-            <tr>
-                <th>{{ $post->user->name }}</th>
-                <th>{{ $post->id }}</th>
-                <td>{{ $post->title }}</td>
-                <td>{{ $post->body }}</td>
-                <td><img src="{{ asset('storage/image/' . $post->image_path) }}" width='200px'></td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container">
+        <div class="col-md-12">
+            @foreach($posts as $post)
+                <div class="card post">
+                    <div class="card-header">
+                        <span>
+                            {{ $post->id . " : " }}
+                        </span>
+                        <span class="post-name">
+                            {{ $post->user->name. " : " }}
+                        </span>
+                        <span>
+                            {{ $post->created_at->format('Y/m/d H:s:i') }}
+                        </span>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            {{ $post->title }}
+                        </div>
+                        <div>
+                            {{ $post->body }}
+                        </div>
+                        @if ($post->image_path)
+                            <div>
+                                <img src="{{ asset('storage/image/' . $post->image_path) }}" width='200px'>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    <div class="container">
 @endsection
